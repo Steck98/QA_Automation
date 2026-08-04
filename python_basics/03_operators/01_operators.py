7a792eb1ae8d583c149a7b771a5ab00caebd712c
Rafał <steczkiewicz1998@Gmail.com>
2026-08-04 17:26:19 +0200

    Feat: add operators

---
+++ python_basics/03_operators/01_operators.py
@@ -0,0 +1,12 @@
+print(17 / 5)
+print(17 // 5)
+print(17 % 5)
+print(5**2)
+print(2**10)
+print(-17 // 5)
+
+
+gold = 137
+bag_volume = 50
+print(f"Full bags: {gold // bag_volume}")
+print(f"Gold left: {gold % bag_volume}")
